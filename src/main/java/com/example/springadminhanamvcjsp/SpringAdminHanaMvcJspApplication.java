32dfb7a816fac85bf7d7cfb9726691dafba03c1e
ChaedongIm <chaedong.im.dev@gmail.com>
2023-07-08 21:27:16 +0900

    Init: 초기세팅

---
+++ src/main/java/com/example/springadminhanamvcjsp/SpringAdminHanaMvcJspApplication.java
@@ -0,0 +1,13 @@
+package com.example.springadminhanamvcjsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAdminHanaMvcJspApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAdminHanaMvcJspApplication.class, args);
+    }
+
+}
